698a0e1ba256c9fce5126e3f577542eeb4d58057
Josh Barker <josh.barker@lab3.com.au>
2021-11-19 12:33:43 +1100

    fix: skip already seen modules

--- pkg/scan/scanner.go
+++ pkg/scan/scanner.go
@@ -8,13 +8,12 @@ import (
 	"github.com/keilerkonzept/terraform-module-versions/pkg/modulecall"
 )
 
-// if the local module search goes more than this number, it will stop searching.
-const MaxRecursionDepth int = 10
-
 type Result struct {
 	ModuleCall tfconfig.ModuleCall
 	Path       string
 }
+
+var foundNestedModules map[string]Result = make(map[string]Result)
 
 func Scan(paths []string, searchLocalModules bool) ([]Result, error) {
 	var out []Result
@@ -37,62 +36,138 @@ func Scan(paths []string, searchLocalModules bool) ([]Result, error) {
 
 	if searchLocalModules {
 		var err error
+		// transform slice of modules to map of modules to identify duplicates
+		modulesByMap := make(map[string]Result)
 
-		out, err = scanLocalModules(out, MaxRecursionDepth)
+		for _, module := range out {
+			p, err := filepath.Abs(module.ModuleCall.Source)
+			if err != nil {
+				return nil, err
+			}
+
+			modulesByMap[p] = module
+		}
+
+		err = scanLocalModules(modulesByMap)
 		if err != nil {
 			return nil, err
+		}
+
+		// transform map of modules to list of modules
+		out = make([]Result, 0)
+		for _, module := range foundNestedModules {
+			out = append(out, module)
 		}
 	}
 
 	return out, nil
 }
 
-//	This function scans for nested terraform modules by parsing local modules recursively.
-func scanLocalModules(modules []Result, depth int) ([]Result, error) {
-	var out []Result
+/*
+	This function scans for nested terraform modules by parsing local modules recursively.
+	The key for the map is the fully qualified path to the source of the module
+	The key is always expanded so that relative paths are not seen a duplicates.
 
-	if depth < 0 {
-		fmt.Printf("[WARN] Max depth of %d was reached. Module dependencies may have a loop that causes infinite recursion.\n", MaxRecursionDepth)
-		return out, nil
+	Consider the following scenario:
+	The naming module has the same name and path, however, they exist in 2 different directories.
+	The source path for module.a.module.naming is : ./mod1/naming
+	The source path for module.b.module.naming is : ./mod2/naming
+
+	main.tf
+	```hcl
+	module "a" {
+		source = "./mod1"
 	}
 
-	for _, m := range modules {
+	module "b" {
+		source = "./mod2"
+	}
+	```
+
+	./mod1:
+	```hcl
+	resource "..." {
+	}
+
+	...
+
+	module "naming" {
+		source = "./naming"
+	}
+	```
+
+	./mod2:
+	```hcl
+		resource "..." {
+	}
+
+	...
+
+	module "naming" {
+		source = "./naming"
+	}
+	```
+*/
+func scanLocalModules(existingModules map[string]Result) error {
+	for baseModuleSource, m := range existingModules {
 		dirPath := filepath.Dir(m.Path)
 		parsed, err := modulecall.Parse(m.ModuleCall)
 		if err != nil {
-			return nil, err
+			return err
 		}
 
-		// if the module source is local, load the nested module and scan for more local modules
+		// if the module source is local, load it and scan for more local modules
 		if parsed.Source.Local != nil {
 			module, err := tfconfig.LoadModule(dirPath + "/" + m.ModuleCall.Source)
 			if err != nil {
-				return nil, fmt.Errorf("read terraform module %q: %w", m.ModuleCall.Source, err)
+				return fmt.Errorf("read terraform module %q: %w", m.ModuleCall.Source, err)
 			}
 
 			for _, call := range module.ModuleCalls {
 				if call == nil {
 					continue
 				}
-				mod := Result{
+				module := Result{
 					Path:       call.Pos.Filename,
 					ModuleCall: *call,
 				}
 
-				singleModule := make([]Result, 0)
-				singleModule = append(singleModule, mod)
-
-				nestedModules, err := scanLocalModules(singleModule, depth-1)
+				// make a fully qualified path to ensure modules are distinct
+				p, err := filepath.Abs(filepath.Join(baseModuleSource, call.Source))
 				if err != nil {
-					return nil, err
+					return err
 				}
 
-				out = append(out, nestedModules...)
+				// if the module has already been seen, skip processing it.
+				if _, ok := foundNestedModules[p]; ok {
+					continue
+				} else {
+					foundNestedModules[p] = module
+				}
+
+				singleModule := make(map[string]Result)
+				singleModule[p] = module
+
+				err = scanLocalModules(singleModule)
+
+				if err != nil {
+					return err
+				}
 			}
 		}
 	}
 
-	result := append(modules, out...)
+	for baseModuleSource, nestedModule := range existingModules {
+		// make a fully qualified path to ensure modules are distinct
+		p, err := filepath.Abs(filepath.Join(baseModuleSource, nestedModule.ModuleCall.Source))
+		if err != nil {
+			return err
+		}
 
-	return result, nil
+		if _, ok := foundNestedModules[p]; !ok {
+			foundNestedModules[p] = nestedModule
+		}
+	}
+
+	return nil
 }
